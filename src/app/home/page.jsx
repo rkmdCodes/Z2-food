7e25cac80b6c18c96c3d348197f785233a2acee9
rkmdCodes <113243168+rkmdCodes@users.noreply.github.com>
2023-07-25 12:28:56 +0530

    distance matrix api integrated && all functionality implemented

--- src/app/home/page.jsx
+++ src/app/home/page.jsx
@@ -8,41 +8,64 @@ import { doc, setDoc, getDoc } from 'firebase/firestore';
 import Carousl from "../carousel/page";
 import LocationPrompt from "../modal/page";
 
-const Container = () => {
-  const [data, setData] = React.useState({
-    name: "",
-    cuisine: "",
-  });
+const api_key = "181256ee-689d-41bb-960c-75cad0c4644f";
+async function getDistance(lat1, lon1, lat2, lon2) {
+  const base_url = `https://graphhopper.com/api/1/route?point=${lat1},${lon1}&point=${lat2},${lon2}&vehicle=car&locale=en&key=${api_key}`;
+  const response = await fetch(base_url);
+  const data = await response.json();
+  console.log(data);
+  const distance = data.paths[0].distance;
+  
+  return distance/1000;
+}
 
+
+
+const Container = () => {
+  const [data, setData] = React.useState([]);
+  let lat = localStorage.getItem("lat");
+  let lon = localStorage.getItem("lon");
   const buttonRef = doc(db, 'outlet', 'Delhi');
 
-const fetchData = async () => {
-  console.log("fetch data is called")
-  try {
-    const docSnapshot = await getDoc(buttonRef);
-    if (docSnapshot.exists()) {
-      const data = docSnapshot.data();
-      console.log('Button document data:', data);
-    } else {
-      console.log('Button document not found!');
+  const fetchData = async () => {
+    console.log("fetch data is called");
+    try {
+      const docSnapshot = await getDoc(buttonRef);
+      if (docSnapshot.exists()) {
+        const da = docSnapshot.data().Res1;
+        console.log("da is = ", da);
+        let filteredData = [];
+        for (var i = 0; i < da.length; i++) {
+          console.log(i, " lat = ", da[i].lat, " long = ", da[i].long);
+          let distanceBet = await getDistance(lat, lon, da[i].lat, da[i].long);
+          console.log("distance = ", distanceBet);
+          if (distanceBet <= 30) {
+            console.log("adding ", i);
+            filteredData.push(da[i]);
+          }
+        }
+        setData(filteredData); // Update the state with the filtered data here
+        console.log('Button document data:', filteredData);
+      } else {
+        console.log('Button document not found!');
+      }
+    } catch (error) {
+      console.error('Error fetching button data:', error);
     }
-  } catch (error) {
-    console.error('Error fetching button data:', error);
-  }
-};
+  };
 
-fetchData();
+// fetchData();
 
 // Call the fetchData function to get the data
 
 
 
 
-  const handleChange = (event) => {
-    setData((prev) => {
-      return { ...prev, [event.target.id]: event.target.value };
-    });
-  };
+  // const handleChange = (event) => {
+  //   setData((prev) => {
+  //     return { ...prev, [event.target.id]: event.target.value };
+  //   });
+  // };
 
   /*  <input
     onChange={(event) => handleChange(event)}
@@ -64,7 +87,7 @@ fetchData();
     <div>
        <Header/>
        <Carousl/>
-   
+       <button onClick={()=>fetchData()}> Fetch</button>
     </div>
   );
 };
